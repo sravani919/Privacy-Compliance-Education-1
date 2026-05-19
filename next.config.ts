bdc77d64dbd5758f901753361ed6f20a51abf204
Sravani Pati <55269953+sravani919@users.noreply.github.com>
2026-05-18 23:31:16 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const repo = "/Privacy-Compliance-Education-1";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/Privacy-Compliance-Education-1" : "",
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? repo : "",
   images: {
     unoptimized: true,
   },
