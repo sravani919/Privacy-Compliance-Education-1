8ea2681f13b40b8d947cd9583db7e3bb3eab0fb9
Sravani Pati <55269953+sravani919@users.noreply.github.com>
2026-05-18 23:26:24 -0400

    Update next.config.ts to modify assetPrefix
    Remove assetPrefix for non-production environments.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/Privacy-Compliance-Education-1" : "",
-  assetPrefix: isProd ? "/Privacy-Compliance-Education-1/" : "",
   images: {
     unoptimized: true,
   },
