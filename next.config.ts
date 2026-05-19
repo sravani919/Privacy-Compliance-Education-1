7552ca3c0a969f384b0ae8b137ecaea9a9d2cad2
Sravani Pati <55269953+sravani919@users.noreply.github.com>
2026-05-18 23:06:46 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,15 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/Privacy-Compliance-Education" : "",
+
+  basePath: isProd
+    ? "/Privacy-Compliance-Education-1"
+    : "",
+
+  assetPrefix: isProd
+    ? "/Privacy-Compliance-Education-1/"
+    : "",
+
   images: {
     unoptimized: true,
   },
